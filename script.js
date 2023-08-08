dda4690f2f7a5238c743a82b6bdb4477d06c9eb2
Amy Mathews <amy.mathews0808@gmail.com>
2023-08-08 12:11:28 -0700

    new changes

--- script.js
+++ script.js
@@ -27,42 +27,53 @@ function toggleSignUp() {
     }
 }
 
+//side bar toggle
+
+const menu_toggle = document.querySelector('.menu-toggle');
+const sidebar = document.querySelector('.sidebar');
+
+menu_toggle.addEventListener('click', () => {
+    menu_toggle.classList.toggle('is-active');
+    sidebar.classList.toggle('is-active');
+});
+
 //Upload image code 
-const uploadArea = document.getElementById('uploadArea');
-const postButton = document.getElementById('postButton');
 
-uploadArea.addEventListener('dragover', (e) => {
-    e.preventDefault();
-    uploadArea.classList.add('dragover');
-});
+    const uploadArea = document.querySelector('.upload-area');
+    const postButton = document.querySelector('.post-button');
 
-uploadArea.addEventListener('dragleave', () => {
-    uploadArea.classList.remove('dragover');
-});
+    uploadArea.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        uploadArea.classList.add('dragover');
+    });
 
-uploadArea.addEventListener('drop', (e) => {
-    e.preventDefault();
-    uploadArea.classList.remove('dragover');
+    uploadArea.addEventListener('dragleave', () => {
+        uploadArea.classList.remove('dragover');
+    });
 
-    const file = e.dataTransfer.files[0];
-    if (file.type.startsWith('image/')) {
-        const img = document.createElement('img');
-        img.src = URL.createObjectURL(file);
-        img.style.maxWidth = '100%';
-        uploadArea.innerHTML = '';
-        uploadArea.appendChild(img);
-    } else {
-        uploadArea.innerHTML = '<p>Unsupported file type</p>';
-    }
-});
+    uploadArea.addEventListener('drop', (e) => {
+        e.preventDefault();
+        uploadArea.classList.remove('dragover');
 
-postButton.addEventListener('click', () => {
-    const img = uploadArea.querySelector('img');
-    if (img) {
-        // Here you can perform the post action
-        alert('Image posted!');
-    }
-});
+        const file = e.dataTransfer.files[0];
+        if (file.type.startsWith('image/')) {
+            const img = document.createElement('img');
+            img.src = URL.createObjectURL(file);
+            img.style.maxWidth = '100%';
+            uploadArea.innerHTML = '';
+            uploadArea.appendChild(img);
+        } else {
+            uploadArea.innerHTML = '<p>Unsupported file type</p>';
+        }
+    });
+
+    postButton.addEventListener('click', () => {
+        const img = uploadArea.querySelector('img');
+        if (img) {
+            // Here you can perform the post action
+            alert('Image posted!');
+        }
+    });
 
 
 
